fae48b04d76e82f29c82d99f432436ddbef56d2b
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-03-23 00:18:07 +0600

    Intro to typedef

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( ) {
+  typedef char Letter;
+  Letter ch;
+  ch = 'a';
+  
+  printf("%c\n",ch);
+  
+  Letter ch1;
+  ch1 = 'i';
+  
+  printf("%c\n",ch1);
+  
+  return 0;
+}
