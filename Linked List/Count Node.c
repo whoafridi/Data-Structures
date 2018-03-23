d8ee8d9bc935f9f13a2009edf033f5986db64563
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-03-24 00:56:43 +0600

    Add files via upload

---
+++ Linked List/Count Node.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct mylist{
+    int data;
+    struct mylist *next;
+}node;
+
+node *create()
+{
+    return (node*)malloc(sizeof(node));
+}
+
+void insert(node *ptr, int data)
+{
+    while(ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = create();
+    ptr->next->data = data;
+    ptr->next->next = NULL;
+}
+
+void countNode(node *ptr)
+{
+    int count = 0;
+    while(ptr->next != NULL)
+    {
+        count++;
+        ptr = ptr->next;
+    }
+    printf("Total %d nodes found.\n", count);
+}
+
+void display(node *ptr)
+{
+    while(ptr->next != NULL)
+    {
+        printf("%d -> ", ptr->next->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    node *head = create();
+    head->next = NULL;
+    printf("The linked list is : \n");
+    insert(head,2);
+    insert(head,4);
+    insert(head,6);
+    insert(head,8);
+    display(head);
+    
+    countNode(head);
+    
+    return 0;
+}
